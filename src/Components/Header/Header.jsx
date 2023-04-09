42724caff6e54f3ad2ce4b1fb984102a3384e1f4
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-04-09 19:06:13 +0600

    navbar responsive and banner section complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
+
+    const [isOpen,setIsOpen] = useState(false)
     return (
         <div className=''>
             <div className='navbar flex justify-between items-center'>
 
                 <div>
-                    <h1>Foodie Shop </h1>
+                    <h1>{isOpen? '' : 'Foodie Shop '}</h1>
                 </div>
 
-                <div>
-                    <Link to='/'>Home</Link>
-                    <Link to='/foods'>Food</Link>
-                    <Link to='about'> About</Link>
-                    <Link to='/login'>Login</Link>
+                <div className='md:flex hidden'>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='/'>Home</NavLink>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='/foods'>Food</NavLink>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='about'> About</NavLink>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='/login'>Login</NavLink>
 
                 </div>
+                <div className='md:hidden'>
+                    <button onClick={()=>setIsOpen(!isOpen)} className='btn btn-info'>x</button>
+
+                </div>
+                {
+                    isOpen &&
+                    
+                    <div className='duration-700 md:hidden flex flex-col left-1 p-10 top-1 absolute bg-opacity-70 bg-yellow-300'>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='/'>Home</NavLink>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='/foods'>Food</NavLink>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='about'> About</NavLink>
+                    <NavLink className={({isActive})=> isActive ? 'text-white' : 'default'} to='/login'>Login</NavLink>
+
+                </div>
+                }
+           
+               
             </div>
+      
         </div>
     );
 };
